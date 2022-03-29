17daf304abacfb3c6bf5330f908593d92a55c135
Afolabi Opakunle <opakunleafolabi@gmail.com>
2022-03-29 22:41:05 +0100

    implemented mongoose

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
 
 const bodyParser = require('body-parser');
 
@@ -8,6 +9,10 @@ const api = require('./routes/api');
 app.use(bodyParser.json());
 
 app.use('/api', api)
+
+const db = 'mongodb+srv://afolabi:8899fifafa@cluster0.bwrba.mongodb.net/eventsdb?retryWrites=true&w=majority';
+
+mongoose.connect(db, (err) => err ? console.log(err) : console.log('Connected to mongoDB'))
 
 app.get('/', (req, res) => {
     res.send("Homepage is bae");
--- server/routes/api.js
+++ server/routes/api.js
@@ -1,9 +1,22 @@
 const express = require('express');
 const router = express.Router();
+const User = require('../models/user');
 
 router.get('/', (req, res) => {
     res.send('From the router API')
-})
+});
+
+router.post('/register', (req, res) => {
+   let userData = req.body;
+   let user = new User(userData);
+   user.save((err, registeredUser) => {
+       if(err) {
+           console.log(err)
+       } else {
+           res.status(200).send(registeredUser)
+       }
+   })
+} )
 
 
 module.exports = router;
